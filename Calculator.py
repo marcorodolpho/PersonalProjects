4a1b819ae2200f655eb78d50905ff4c251b7d0ed
marcorodolpho <marco.rodolpho@gmail.com>
2025-03-30 22:59:31 -0300

    feat: testing tkinter for the first time

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,30 @@
 #This is the first project to learn new habilities
 
+import tkinter
 
-print("Initializing the project on github. ")
+app = tkinter.Tk()
+
+app.title = ("My calculator")
+
+entry1 = tkinter.Entry(app)
+entry1.pack()
+
+entry2 = tkinter.Entry()
+entry2.pack()
+
+_sum_button = tkinter.Button(app, text="Soma")
+_sum_button.pack()
+
+sub_button = tkinter.Button(app, text="Sub")
+sub_button.pack()
+
+mult_button = tkinter.Button(app, text="Mult")
+mult_button.pack()
+
+divd_button = tkinter.Button(app, text="Divd")
+divd_button.pack()
+
+result_button = tkinter.Button(app, text="Igual")
+result_button.pack()
+
+app.mainloop()
